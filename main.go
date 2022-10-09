fbebcfa975ed9c9f8629456d1f94f29502de66bb
edarcode <edarcode@gmail.com>
2022-10-09 03:12:12 -0500

    init server

--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	server := echo.New()
 	server.GET("/", func(context echo.Context) error {
-		return context.String(http.StatusOK, "¡Hola, edarcode!")
+		return context.String(200, "¡Hola, edarcode!")
 	})
 	server.Logger.Fatal(server.Start(":3000"))
 }
